41b4d04e995af1188c2c239aa4da0978b26f5750
Yavor Ivanov <yavor.ivanov@sap.com>
2017-06-08 16:26:29 +0300

    Fill Detail and DetailDetail view with static content

--- webapp/controller/DetailDetail.controller.js
+++ webapp/controller/DetailDetail.controller.js
@@ -6,6 +6,10 @@ sap.ui.define([
 
 	return Controller.extend("ui5confUI5Conf.controller.DetailDetail", {
 		onInit: function () {
+		},
+		toggleFooter: function () {
+			var oObjectPageLayout = this.getView().byId("ObjectPageLayout");
+			oObjectPageLayout.setShowFooter(!oObjectPageLayout.getShowFooter());
 		}
 	});
 });
