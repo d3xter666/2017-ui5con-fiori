14b4107be377eb8ff7636e08131330e6d56d5e5e
Yavor Ivanov <yavor.ivanov@sap.com>
2017-06-08 16:56:20 +0300

    Add data to Detail and DetailDetail

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,19 +1,45 @@
 sap.ui.define([
 	"jquery.sap.global",
-	"sap/ui/core/mvc/Controller"
-], function (jQuery, Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function (jQuery, Controller, JSONModel) {
 	"use strict";
 
 	return Controller.extend("ui5confUI5Conf.controller.Detail", {
 		onInit: function () {
+			this.getRouter().attachRouteMatched(this._handleRouteChanged.bind(this));
 		},
+		_itemDetail: null,
+		_handleRouteChanged: function (oEvent) {
+			var oView = this.oView,
+				oModel = this.oView.getModel(),
+				oData = oModel.getData(),
+				sProductId = oEvent.getParameter("arguments")["detail-item"],
+				oItemData = oData["ProductCollection"].filter(function (oCurItem) {
+					return oCurItem.ProductId === sProductId;
+				})[0];
+
+			this._itemDetail = sProductId;
+
+			if (oItemData) {
+				oView.setModel(new JSONModel(oItemData), "detail");
+			}
+		},
+
 		getRouter: function () {
 			return sap.ui.core.UIComponent.getRouterFor(this);
 		},
-		handlePress: function () {
+		handlePress: function (oEvent) {
+			var sId = oEvent.getParameter("id"),
+				oSelectedItem = sap.ui.getCore().byId(sId),
+				// It's important to point that we're getting the second model, not the default one
+				oModel = oSelectedItem.getModel("detailDetail"),
+				sPath = oSelectedItem.getBindingContextPath(),
+				oData = oModel.getProperty(sPath);
+
 			this.getRouter().navTo("detailDetailName", {
-				"detail-detail-item": "item-inner1",
-				"detail-item": "item1"
+				"detail-detail-item": oData.ProductId,
+				"detail-item": this._itemDetail
 			});
 		},
 		toggleFooter: function () {
--- webapp/controller/DetailDetail.controller.js
+++ webapp/controller/DetailDetail.controller.js
@@ -1,11 +1,31 @@
 sap.ui.define([
 	"jquery.sap.global",
-	"sap/ui/core/mvc/Controller"
-], function (jQuery, Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function (jQuery, Controller, JSONModel) {
 	"use strict";
 
 	return Controller.extend("ui5confUI5Conf.controller.DetailDetail", {
 		onInit: function () {
+			this.getRouter().attachRouteMatched(this._handleRouteChanged.bind(this));
+		},
+		getRouter: function () {
+			return sap.ui.core.UIComponent.getRouterFor(this);
+		},
+		_itemDetail: null,
+		_handleRouteChanged: function (oEvent) {
+			var oView = this.oView,
+				// Be aware to fetch the proper model!
+				oModel = this.oView.getModel("detailDetail"),
+				oData = oModel.getData(),
+				sProductId = oEvent.getParameter("arguments")["detail-detail-item"],
+				oItemData = oData["ProductCollection"].filter(function (oCurItem) {
+					return oCurItem.ProductId === sProductId;
+				})[0];
+
+			if (oItemData) {
+				oView.setModel(new JSONModel(oItemData), "detail");
+			}
 		},
 		toggleFooter: function () {
 			var oObjectPageLayout = this.getView().byId("ObjectPageLayout");
